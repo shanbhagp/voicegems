52a193d4af63fe7e9842d178182d071b516366eb
Praveen R. Shanbhag <shanbhagp@aol.com>
2012-11-02 02:14:34 -0700

    try again to rollback mail interceptor change

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,9 +1,7 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
-     
-       message.subject = "#{message.to} #{message.subject}"
-       message.to = "shanbhagp@aol.com"
-   
+    message.subject = "#{message.to} #{message.subject}"
+    message.to = "shanbhagp@aol.com"
   end
 end
 
