5f3fc9b271012115e661b8ea7f50a17be23538d6
Praveen R. Shanbhag <shanbhagp@aol.com>
2012-11-02 14:19:42 -0700

    rollback last commmit, default host

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,4 @@
 class UserMailer < ActionMailer::Base
-  default_url_options[:host] = request.host_with_port
   default from: "shanbhagp@aol.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
