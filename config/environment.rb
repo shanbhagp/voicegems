c5574e984611b31871f7551548b89d094a2aa7cc
Praveen R. Shanbhag <shanbhagp@aol.com>
2012-11-02 02:40:48 -0700

    configure heroku mail in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 App4::Application.initialize!
+
+#Action Mailer configuration for Heroku
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.aol.com",
+  :port  => 25,
+  :user_name  => "shanbhagp@aol.com",
+  :password  => "4oceana7ddd",
+  :authentication  => :login
+}
+
+config.action_mailer.raise_delivery_errors = true
