a537a967632a15568cee2c143945ff698e8494c6
Praveen R. Shanbhag <shanbhagp@aol.com>
2012-11-05 00:43:35 -0800

    another attempt at routing again

--- config/routes.rb
+++ config/routes.rb
@@ -123,7 +123,7 @@ App4::Application.routes.draw do
  match '/test', to: 'users#test'
 
  #match '/events/assets/' => redirect('/assets')
- match '/events/assets/:etc' => redirect('/assets/%{etc}')
+ match '/events/assets/*etc' => redirect('/assets/%{etc}')
 #match "/stories/:name" => redirect("/posts/%{name}")
 
  #match '/hide/:id', to: 'practiceobjects#destroy', via: :destroy
