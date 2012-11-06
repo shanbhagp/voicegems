6b1ce5ab6e7056726f53bcdbec6da3320081e732
Praveen R. Shanbhag <shanbhagp@aol.com>
2012-11-05 16:59:16 -0800

    using symbol

--- config/routes.rb
+++ config/routes.rb
@@ -127,8 +127,10 @@ App4::Application.routes.draw do
  #match '/events/assets/' => redirect('/assets')
 # match '/events/assets/etc' => redirect('/assets/%{etc}')
 #match "/stories/:name" => redirect("/posts/%{name}")
- #match '/events/assets/*etc' => redirect('/assets/%{etc}')
- match '/events/assets/aplayer/*etc' => redirect('/assets/aplayer/%{etc}')
+ #match '/events/assets/*etc' => redirect('/assets/%{etc}')  #GIVES partial path
+ #match '/events/assets/aplayer/*etc' => redirect('/assets/aplayer/%{etc}')  # gives same old path now! wtf?
+ match '/events/assets/aplayer/:a' => redirect('/assets/aplayer/%{a}')
+
 #match '/events/assets/aplayer/*a' => redirect('/assets/aplayer/%{a}'), :requirements => { :a => /.*/ }
 #match '/events/assets/aplayer/audiojs.swf?playerInstance=audiojs/*a' => redirect('/assets/aplayer/audiojs.swf?playerInstance=audiojs%{a}'), :constraints => { :a => /.*/ }
 #match '/events/assets/aplayer/*a' => redirect('/assets/aplayer/%{a}'), :constraints => { :a => /.*/ }
