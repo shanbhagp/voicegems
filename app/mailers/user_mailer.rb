c66dde21647253e3ff72a2b0d282cba23e547c2d
Praveen R. Shanbhag <shanbhagp@aol.com>
2012-11-08 12:15:39 -0800

    change default from in usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "shanbhagp@aol.com"
+  default from: "passwords@namecoach.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
