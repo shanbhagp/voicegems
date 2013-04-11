c075c236c9e46427cdd66c23430d89eaf4b99918
Praveen R. Shanbhag <shanbhagp@aol.com>
2013-04-10 23:02:46 -0700

    added requires

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require jquery.purr
+//= require jquery.cslider
+//= require jquery.flexslider-min
 //= require best_in_place
 //= require bootstrap
 //= provide jrec
