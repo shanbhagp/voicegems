59d6da2e7aece3169e82157136b30ed6ceaa0961
Praveen R. Shanbhag <shanbhagp@aol.com>
2013-06-27 02:23:29 -0700

    changed to new dev mail account, minor grad and home page fixes, temporary warning on record and record_step2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,8 +47,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "prshanbhag",
-  :password             => "4oceana7ddd",
+  :user_name            => "ncwbdev",
+  :password             => "bugsbegone101",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
