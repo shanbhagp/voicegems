2ef9e2d86064ad0da84d1acb30a86aeffbe8d4f3
Praveen R. Shanbhag <shanbhagp@aol.com>
2013-08-04 18:00:31 -0700

    changed button to SAVE and put in jia's new flashdetector code, again

--- app/assets/javascripts/flashblock-detector.js
+++ app/assets/javascripts/flashblock-detector.js
@@ -1,27 +1,21 @@
 flashBlockDetect = function(callbackMethod){
   var return_value = 0;
-	if ( navigator.plugins["Shockwave Flash"] ) {
-		if (navigator.userAgent.indexOf('MSIE') > -1) {
-			try {
-			  new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
-			} catch (e) {
-			  return_value = 2;
-			}
-		} else {
-		  embed_length = $('embed').length;
-			object_length = $('object').length;
-			if( (embed_length > 0) || (object_length > 0) ){
-				/* Mac / Chrome using FlashBlock + Mac / Safari using AdBlock */
-				$('object, embed').each(function() {
-				  if( $(this).css('display') === 'none' ){
+  	if (navigator.appName == 'Microsoft Internet Explorer') {
+		return_value = 0;
+	} else if ( navigator.plugins["Shockwave Flash"] ) {
+		embed_length = $('embed').length;
+		object_length = $('object').length;
+		if( (embed_length > 0) || (object_length > 0) ){
+			/* Mac / Chrome using FlashBlock + Mac / Safari using AdBlock */
+			$('object, embed').each(function() {
+				if( $(this).css('display') === 'none' ){
 				    return_value = 2;
-					}
-				});			
-			} else {
-				/* Mac / Firefox using FlashBlock */
-				if( $('div[bginactive]').length > 0 ){
-				  return_value = 2;
 				}
+			});			
+		} else {
+			/* Mac / Firefox using FlashBlock */
+			if( $('div[bginactive]').length > 0 ){
+				return_value = 2;
 			}
 		}
 	} else {
@@ -31,6 +25,6 @@ flashBlockDetect = function(callbackMethod){
   if (callbackMethod && typeof(callbackMethod) === "function") {
 		callbackMethod(return_value);
 	} else {
-	  return return_value;
+		return return_value;
 	}
 }
