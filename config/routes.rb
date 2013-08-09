9520f9edf6f2d4ac203328c90e19ef4fef9bd2e5
Praveen R. Shanbhag <shanbhagp@aol.com>
2013-08-09 11:40:25 -0700

    demo_record_temp change

--- config/routes.rb
+++ config/routes.rb
@@ -316,7 +316,7 @@ match '/migrate_entries', to: 'events#migrate_entries'
 
 match '/flashdetect', to: 'static#flashdetect'
 
-match '/demo_record_temp', to: 'events#demo_record_temp', as: '/demo_record_temp'
+match '/demo_record_temp/:event_code', to: 'events#demo_record_temp', as: '/demo_record_temp'
 
 #match '/show_master', to: 'events#show_master'
 
