d0097cc64bf78cf56129797b97998d8313f8423b
Praveen R. Shanbhag <shanbhagp@aol.com>
2013-08-22 00:38:04 -0700

    another cache buster attempt

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -896,9 +896,10 @@ end
 
 
   def set_cache_buster
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+        headers["Pragma"] = "no-cache"
+        headers["Cache-Control"] = "must-revalidate"   
+        headers["Cache-Control"] = "no-cache"   
+        headers["Cache-Control"] = "no-store"      
   end
 
 end
