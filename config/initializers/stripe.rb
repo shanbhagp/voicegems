f5438dcca661d099373879faa17c3d319d032779
shanbhagp <shanbhagp@aol.com>
2013-11-07 17:36:14 -0800

    stripe changes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 
 if Rails.env.development?
-  ENV['PUBLISHABLE_KEY'] = "pk_wLJXPbCWbzbWqjFCbvANNQ7cY2HaE"
-  ENV['SECRET_KEY'] = "sk_UwOPeoexW4SG2vXxG75VPff9jnW5D"
+  ENV['PUBLISHABLE_KEY'] = "pk_test_PK56I8xRA5vMzyn7yhIQC6wk"
+  ENV['SECRET_KEY'] = "sk_test_tzVXjCcObQMKQMPvstAX3hrs"
 end
 
 Rails.configuration.stripe = {
