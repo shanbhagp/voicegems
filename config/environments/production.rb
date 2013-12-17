e6150e5c58dd56594c3fc708fbf1b18dd1516419
shanbhagp <shanbhagp@aol.com>
2013-12-16 23:43:02 -0800

    changed cloudfront host url for testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ App4::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-   #config.action_controller.asset_host = "https://d3guq2utxkoa0j.cloudfront.net"
+   #config.action_controller.asset_host = "https://d3guq2utxkoa0j555.cloudfront.net"
    #config.font_assets.origin = 'http://voicegems.com'
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
