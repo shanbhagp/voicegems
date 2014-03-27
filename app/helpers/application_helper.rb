59614cc4e79d4a48442a5339b0e75216b71a7ff0
Peeyush Aggarwal <peeyush@name-coach.com>
2014-03-27 01:03:27 -0700

    Fixed forms.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
-
-	
-
+   ALERT_CONVERSION_MAP = { :error => 'danger' }
+   def get_alert_class(alert_type)
+      return ALERT_CONVERSION_MAP[alert_type] if ALERT_CONVERSION_MAP.has_key?(alert_type)
+      alert_type
+   end
 end
