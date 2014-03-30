71d6481273dcd5c5e118faaf0cf07c8389d86636
Peeyush Aggarwal <peeyush@name-coach.com>
2014-03-29 20:26:49 -0700

    Fixed modal windows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-   ALERT_CONVERSION_MAP = { :error => 'danger' }
+   ALERT_CONVERSION_MAP = { :error => 'danger', :notice => 'info' }
    def get_alert_class(alert_type)
       return ALERT_CONVERSION_MAP[alert_type] if ALERT_CONVERSION_MAP.has_key?(alert_type)
       alert_type
