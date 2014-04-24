968fc6fab38752f6c4caceaf6972c21347ffdeae
shanbhagp <shanbhagp@aol.com>
2014-04-24 03:21:18 -0700

    somehow changed voicegems.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -172,16 +172,6 @@ ActiveRecord::Schema.define(:version => 20140424072642) do
     t.boolean  "unlimited"
   end
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
   create_table "subscriptions", :force => true do |t|
     t.integer  "user_id"
     t.string   "email"
@@ -236,6 +226,7 @@ ActiveRecord::Schema.define(:version => 20140424072642) do
     t.string   "event_type"
     t.string   "grad_date"
     t.string   "coupon"
+    t.string   "registration_code"
     t.string   "promo_coupon"
   end
 
